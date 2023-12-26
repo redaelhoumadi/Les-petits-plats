b6ff07670097db7383f3f5fc0575f231bd5ae6aa
redaelhoumadi <reda.elhoumadi@gmail.com>
2023-12-26 13:31:57 +0100

    Add files via upload

--- js/pages/index.js
+++ js/pages/index.js
@@ -1,82 +1,27 @@
 import {recipes} from '../../data/recipes.js'
+import { allIngredients } from '../select.js';
+import { allAppareils } from '../select.js';
+import { allUstensils } from '../select.js';
+import {custumSelect} from '../select.js';
+import { tagsSearch } from '../select.js';
+import { tableauValeurs } from '../select.js';
+import { hasAllValues } from '../select.js';
 
-function allIngredients(){
-    const select = document.getElementById('ingredients')
-  
-  // Utilise la méthode .flat() pour aplatir le tableau de tableaux en un seul tableau
-  const allIngredients = recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient));
-  
-  
-  
-  const uniqueIngredients = [...new Set(allIngredients)];
-  
-  // Trier les ingrédients par ordre alphabétique
-  const sortedIngredients = uniqueIngredients.sort((a, b) => a.localeCompare(b));
-  
-  // Affiche chaque ingrédient dans la console
-  sortedIngredients.forEach((ingredient) => {
-      const ingrediente = ingredient
-      const option = document.createElement('option');
-      option.innerHTML = ingrediente
-      select.appendChild(option)
-  });
-  
-  }
-  
-  function allAppareils(){
-    const select = document.getElementById('appareils')
-  
-        // Extraction de tous les appareils de toutes les recettes
-  const allAppliances = [...new Set(recipes.map(recipe => recipe.appliance))];
-  const sortedallAppliances = allAppliances.sort((a, b) => a.localeCompare(b));
-  
-  sortedallAppliances.forEach((recipe) => {
-    const Appareil = recipe
-  
-      const option = document.createElement('option');
-      option.innerHTML = Appareil
-      select.appendChild(option)
-      
-    }) 
-    
-  
-  }
-  
-  function allUstensils(){
-    const select = document.getElementById('Ustensiles')
-  
-        // Extraction de tous les appareils de toutes les recettes
-        const allUstensils = [...new Set(recipes.flatMap(recipe => recipe.ustensils))];
-        const sortedallUstensilss = allUstensils.sort((a, b) => a.localeCompare(b));
-  
-  sortedallUstensilss.forEach((recipe) => {
-    const Ustensilss = recipe
-  
-      const option = document.createElement('option');
-      option.innerHTML = Ustensilss
-      select.appendChild(option)
-      
-    }) 
-    
-  
-  }
-  
-
-
+const ulSelect = document.querySelectorAll('.mg-custom-select__list');
+const totalRecipesLabel =document.querySelector('.filter-results');
 const recipesContainer = document.querySelector('.recipt-contenaire');
-const totalRecipesLabel = document.querySelector('.filter-results');
+const optionIngredient = document.getElementById('ingredients')
+const optionAppareils = document.getElementById('appareils')
+const optionUstensiles = document.getElementById('Ustensiles')
+export let resultsSearch = ''
 
-function searchForm(){
     
-    function displayTotalRecipes(){
-        totalRecipesLabel.textContent = recipes.length +' recettes';
+     export function displayTotalRecipes(total){
+        totalRecipesLabel.textContent = total.length +' recettes';
     }
 
-    function cleanTotalRecipes(){
-        totalRecipesLabel.textContent= '';
-    }
 
-    function displayRecipes(){
+    export function displayRecipes(){
         recipes.forEach(recipe => {
             const Template = new recipeCard(recipe)
             recipesContainer.appendChild(Template.createRecipeCard())
@@ -84,12 +29,31 @@ function searchForm(){
         })
     }
 
-    function cleanRecipescontenair(){
+    export function cleanRecipescontenair(){
         recipesContainer.innerHTML ='';
     }
-    
-    function search() {
 
+    export function displayResults(res){
+        cleanRecipescontenair()
+        res.forEach(recipe => {
+            const Template = new recipeCard(recipe)
+            recipesContainer.appendChild(Template.createRecipeCard())
+            
+        })
+
+    }
+
+    export function clearSelect(){
+        optionIngredient.innerHTML = ''
+        optionUstensiles.innerHTML = ''
+        optionAppareils.innerHTML = ''
+        ulSelect.innerHTML = ''
+    }
+    
+    function searchInput() {
+
+        const mainContainer = document.querySelector('.main-container')
+        const recipesContainerErreur = document.createElement('div')
         const clearSearch = document.querySelector('.clear-search')
         const searchIcon = document.querySelector('.submit')
 
@@ -97,10 +61,11 @@ function searchForm(){
             const query = event.target.value.toLowerCase()
             ;
             if (query.length >= 3) {
-                const results = recipes.filter(recipe => {
+
+                resultsSearch = recipes.filter(recipe => {
                     const hasMatchingIngredient = recipe.ingredients.some(ingredient =>
                         ingredient.ingredient.toLowerCase().includes(query)
-                    );
+                    )
             
                     const hasMatchingName = recipe.name.toLowerCase().includes(query);
 
@@ -109,38 +74,55 @@ function searchForm(){
                     return hasMatchingIngredient || hasMatchingName || hasMatchingDescription;
                     
                 });
-                cleanTotalRecipes()
-                totalRecipesLabel.textContent = results.length +' recettes';
-
+                
+                
+                displayTotalRecipes(resultsSearch)
                 searchIcon.classList.add('submitactive')
                 clearSearch.style.display = 'block'
+                clearSelect()
+                displayResults(resultsSearch)
+                allIngredients(resultsSearch)
+                allAppareils(resultsSearch)
+                allUstensils(resultsSearch)
                 
+                custumSelect()
                 
-                function displayResults(){
-                    cleanRecipescontenair()
-                    results.forEach(recipe => {
-                        const Template = new recipeCard(recipe)
-                        recipesContainer.appendChild(Template.createRecipeCard())
-                        
-                    })
-
+                if (resultsSearch.length === 0) {
+                    
+                    recipesContainerErreur.classList.add('recipes-container-erreur')
+                    recipesContainerErreur.style.display = 'block';
+                    recipesContainerErreur.classList.add('text-center')
+                    recipesContainerErreur.textContent = ' Aucune recette ne contient ' + query + ' vous pouvez chercher tarte aux pommes , poisson, etc'
+                    mainContainer.appendChild (recipesContainerErreur)
                 }
-                displayResults()
-            }
+
+                else if (resultsSearch.length > 0 && tableauValeurs.length > 0) {
+                tagsSearch(tableauValeurs, resultsSearch)}
+
+                else {
+                    console.log('erreur');
+                }
+            } 
             
+            
+
             else {
                 searchIcon.classList.remove('submitactive')
                 clearSearch.style.display = 'none'
                 cleanRecipescontenair()
                 displayRecipes(recipes)
                 displayTotalRecipes(recipes)
-                
+                clearSelect()
+                allIngredients(recipes)
+                allAppareils(recipes)
+                allUstensils(recipes)
+                custumSelect()  
             } 
         })
     }
 
-    search()
-    displayTotalRecipes()
-}
+    searchInput()
+    displayTotalRecipes(recipes)
 
-searchForm()
+
+
--- js/select.js
+++ js/select.js
@@ -1,135 +1,220 @@
-import {recipes} from 'data/recipes.js'
+import {recipes} from '../data/recipes.js'
+import { displayResults } from './pages/index.js';
+import { clearSelect } from './pages/index.js';
+import { cleanRecipescontenair } from './pages/index.js';
+import { displayTotalRecipes } from './pages/index.js';
+import { resultsSearch} from './pages/index.js';
 
-function allIngredients(){
-  const select = document.getElementById('ingredients')
+export function allIngredients(recipes) {
+  const select = document.getElementById('ingredients');
 
-// Utilise la méthode .flat() pour aplatir le tableau de tableaux en un seul tableau
-const allIngredients = recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient));
+  // Utilise la méthode .flat() pour aplatir le tableau de tableaux en un seul tableau
+  const allIngredients = recipes.flatMap(recipe => recipe.ingredients.map(ingredient => ingredient.ingredient.toLowerCase()));
 
+  // Utilise Set directement pour obtenir des valeurs uniques
+  const uniqueIngredients = [...new Set(allIngredients)];
 
+  // Trier les ingrédients par ordre alphabétique
+  uniqueIngredients.sort((a, b) => a.localeCompare(b));
 
-const uniqueIngredients = [...new Set(allIngredients)];
+  // Utilise document.createDocumentFragment pour améliorer les performances lors de l'ajout d'options
+  const fragment = document.createDocumentFragment();
 
-// Trier les ingrédients par ordre alphabétique
-const sortedIngredients = uniqueIngredients.sort((a, b) => a.localeCompare(b));
-
-// Affiche chaque ingrédient dans la console
-sortedIngredients.forEach((ingredient) => {
-    const ingrediente = ingredient
+  const firstOption = document.createElement('option');
+  firstOption.classList.add('btn-select')
+  firstOption.textContent = 'Ingredients'
+  firstOption.disabled = true;
+  firstOption.selected = true;
+  select.appendChild(firstOption);
+  // Utilise forEach avec la déstructuration des paramètres
+  uniqueIngredients.forEach((ingredient) => {
     const option = document.createElement('option');
-    option.innerHTML = ingrediente
-    select.appendChild(option)
-});
+    option.classList.add('second');
+    option.textContent = ingredient;
+    fragment.appendChild(option);
+  });
 
+  // Ajoute le fragment complet en une seule opération
+  select.appendChild(fragment);
 }
 
-function allAppareils(){
-  const select = document.getElementById('appareils')
+export function allAppareils(recipes) {
+  const select = document.getElementById('appareils');
 
-      // Extraction de tous les appareils de toutes les recettes
-const allAppliances = [...new Set(recipes.map(recipe => recipe.appliance))];
-const sortedallAppliances = allAppliances.sort((a, b) => a.localeCompare(b));
+  // Utilise Set directement pour obtenir des valeurs uniques
+  const uniqueAppliances = [...new Set(recipes.map(recipe => recipe.appliance.toLowerCase()))];
 
-sortedallAppliances.forEach((recipe) => {
-  const Appareil = recipe
+  // Trie les appareils par ordre alphabétique
+  uniqueAppliances.sort((a, b) => a.localeCompare(b));
 
+  // Utilise document.createDocumentFragment pour améliorer les performances lors de l'ajout d'options
+  const fragment = document.createDocumentFragment();
+
+  const firstOption = document.createElement('option');
+  firstOption.classList.add('btn-select')
+  firstOption.textContent = 'Appareils'
+  firstOption.disabled = true;
+  firstOption.selected = true;
+  select.appendChild(firstOption);
+
+  // Utilise forEach avec la déstructuration des paramètres
+  uniqueAppliances.forEach((appliance) => {
     const option = document.createElement('option');
-    option.innerHTML = Appareil
-    select.appendChild(option)
-    
-  }) 
+    option.textContent = appliance;
+    fragment.appendChild(option);
+  });
+
+  // Ajoute le fragment complet en une seule opération
+  select.appendChild(fragment);
+}
+
+
+export function allUstensils(recipes) {
+  const select = document.getElementById('Ustensiles');
+
+  // Utilise Set directement pour obtenir des valeurs uniques
+  const uniqueUstensils = [...new Set(
+    recipes.flatMap(recipe => recipe.ustensils.map(ustensil => ustensil.toLowerCase()))
+  )];
+
+  // Trie les ustensiles par ordre alphabétique
+  uniqueUstensils.sort((a, b) => a.localeCompare(b));
+
+  // Utilise document.createDocumentFragment pour améliorer les performances lors de l'ajout d'options
+  const fragment = document.createDocumentFragment();
+
+  const firstOption = document.createElement('option');
+  firstOption.classList.add('btn-select')
+  firstOption.textContent = 'Ustensiles'
+  firstOption.disabled = true;
+  firstOption.selected = true;
+  select.appendChild(firstOption);
+
+  // Utilise forEach avec la déstructuration des paramètres
+  uniqueUstensils.forEach((ustensil) => {
+    const option = document.createElement('option');
+    option.textContent = ustensil;
+    fragment.appendChild(option);
+  });
+
+  // Ajoute le fragment complet en une seule opération
+  select.appendChild(fragment);
+}
+
+allIngredients(recipes)
+allAppareils(recipes)
+allUstensils(recipes)
+
+
+export let tableauValeurs = [];
+export let selectValue ='';
+export let hasAllValues=''
+
+
+export function custumSelect(){
+// Sélection de tous les éléments avec la classe 'mg-custom-select-js'
+let parents = Array.from(document.querySelectorAll('.mg-custom-select-js'));
+
+// Pour chaque élément trouvé
+parents.forEach(function(item) {
+  // Le parent actuel
+  const parent = item;
   
+  // Vérifier si la classe 'mg-custom-select--search-js' est présente
+  let withSearch = parent.classList.contains('mg-custom-select--search-js');
 
-}
-
-function allUstensils(){
-  const select = document.getElementById('Ustensiles')
-
-      // Extraction de tous les appareils de toutes les recettes
-      const allUstensils = [...new Set(recipes.flatMap(recipe => recipe.ustensils))];
-      const sortedallUstensilss = allUstensils.sort((a, b) => a.localeCompare(b));
-
-sortedallUstensilss.forEach((recipe) => {
-  const Ustensilss = recipe
-
-    const option = document.createElement('option');
-    option.innerHTML = Ustensilss
-    select.appendChild(option)
-    
-  }) 
-  
-
-}
-
-allIngredients()
-allAppareils()
-allUstensils()
-
-//custom select dropdown
-var parents = Array.from(document.querySelectorAll('.mg-custom-select-js'));
-parents.forEach(function(item, index) {
-  var parent = item;
-  var withSearch = parent.classList.contains('mg-custom-select--search-js');
-
-  var customSelect = document.createElement('div');
+  // Créer l'élément de sélection personnalisé (customSelect)
+  let customSelect = document.createElement('div');
   customSelect.classList.add('mg-custom-select');
 
-  var customSelect__invoker = document.createElement('button');
+  // Créer le bouton d'invocation (customSelect__invoker)
+  let customSelect__invoker = document.createElement('button');
+  const btnSelect = document.querySelectorAll('.btn-select');
   customSelect__invoker.textContent = parent.childNodes[1].options[parent.childNodes[1].selectedIndex].text;
   customSelect__invoker.classList.add('mg-custom-select__button');
   customSelect.appendChild(customSelect__invoker);
 
-  var dropdown = document.createElement('div');
+  // Créer le conteneur du menu déroulant
+  let dropdown = document.createElement('div');
   dropdown.classList.add('mg-custom-select__dropdown');
 
+  // Ajouter la barre de recherche si nécessaire
   if(withSearch) {
     var searchInput = document.createElement('input');
     searchInput.classList.add('mg-custom-select__search');
     searchInput.setAttribute('type', 'text');
-    
 
-    var searchInputLabel = document.createElement('label');
+    let searchInputLabel = document.createElement('label');
     searchInputLabel.classList.add('mg-custom-select__search__label');
     searchInputLabel.appendChild(searchInput);
 
     dropdown.appendChild(searchInputLabel);
   }
 
-  var customSelect__list = document.createElement('ul');
+  // Créer la liste d'options du menu déroulant
+  let customSelect__list = document.createElement('ul');
   customSelect__list.classList.add('mg-custom-select__list');
-  var selectOption = Array.from(parent.querySelectorAll('option'));
+  let selectOption = Array.from(parent.querySelectorAll('option'));
   selectOption.forEach(function(item, index) {
-    if ( index == 0) {
+    if (index == 0) {
       return;
     }
-    var customSelect__item = document.createElement('li');
+    let customSelect__item = document.createElement('li');
+    
     customSelect__item.classList.add('mg-custom-select__list__item')
     customSelect__item.dataset.value = item.value;
     customSelect__item.textContent = item.text;
     customSelect__list.appendChild(customSelect__item);
+    
+
   })
 
+  // Assembler tous les éléments
   dropdown.appendChild(customSelect__list);
   customSelect.appendChild(dropdown);
   parent.appendChild(customSelect);
   parent.childNodes[1].style.display = 'none';
+
+  // Récupérer la durée de l'animation de transition
+  let duration = parseFloat(window.getComputedStyle(dropdown, null).getPropertyValue('transition-duration')) * 1000;
   
-  var duration = parseFloat(window.getComputedStyle(dropdown, null).getPropertyValue('transition-duration')) * 1000;
-  
-  var customSelect__items =  Array.from(customSelect__list.querySelectorAll('li'));
+  // Sélectionner tous les éléments de la liste déroulante
+  let customSelect__items = Array.from(customSelect__list.querySelectorAll('li'));
   customSelect__items.forEach(function(item, index) {
+    // Ajouter un écouteur d'événement 'click' à chaque élément de la liste
     item.addEventListener('click', function() {
-      var selectedOption = parent.childNodes[1].options[parent.childNodes[1].selectedIndex];
+      let selectedOption = parent.childNodes[1].options[parent.childNodes[1].selectedIndex];
       dropdown.classList.toggle('opened');
       parent.childNodes[1].value = this.dataset.value;
+      let tags = document.createElement('div');
+      const tagsButton = document.createElement('button');
+      const tagsLabel = document.createElement('h3');
+      tags.classList.add('tags')
+      tagsButton.classList.add('tags-button');
+      const tagContnair = document.getElementById('selected-tags')
+      tagsLabel.innerHTML = this.dataset.value;
+      tags.appendChild(tagsLabel);
+      tags.appendChild(tagsButton);
+      
+      tagContnair.appendChild(tags);
+      selectValue= this.dataset.value;
+      updateTags(selectValue)
+      if (resultsSearch.length > 0){
+        tagsSearch(tableauValeurs,resultsSearch)
+      }
+      else if (resultsSearch.length === 0){
+        tagsSearch(tableauValeurs,recipes)
+      }
 
       selectedOption.removeAttribute('selected');
       parent.childNodes[1].options[index + 1].setAttribute('selected', true);
 
-      var selectChangeEvent = document.createEvent("HTMLEvents");
+      // Déclencher un événement de changement
+      let selectChangeEvent = document.createEvent("HTMLEvents");
       selectChangeEvent.initEvent('change', false, true);
       parent.childNodes[1].dispatchEvent(selectChangeEvent);
 
+      // Mettre à jour le texte du bouton d'invocation et réinitialiser la recherche (le cas échéant)
       customSelect__invoker.textContent = this.textContent;
       setTimeout(function() {
         if(withSearch) {
@@ -142,10 +227,11 @@ parents.forEach(function(item, index) {
     })
   })
 
+  // Ajouter un écouteur d'événement 'keyup' à la barre de recherche (le cas échéant)
   if(withSearch) {
     searchInput.addEventListener('keyup', function() {
-      var inputVal = this.value;
-      var pattern = new RegExp(inputVal, 'i');
+      let inputVal = this.value;
+      let pattern = new RegExp(inputVal, 'i');
       customSelect__items.forEach(function(item, index) {
         if (!item.textContent.match(pattern)) {
           item.classList.add('hidden');
@@ -156,13 +242,71 @@ parents.forEach(function(item, index) {
     })
   }
 
+  // Ajouter un écouteur d'événement 'click' au bouton d'invocation
   customSelect__invoker.addEventListener('click', function() {
     dropdown.classList.toggle('opened');
   })
 
+  // Ajoutez un écouteur d'événements pour le clic sur le document
+document.addEventListener('click', function(event) {
+  // Vérifiez si l'élément cliqué est à l'intérieur du menu déroulant
+  if (!dropdown.contains(event.target) && !customSelect__invoker.contains(event.target)) {
+      // Caché le menu déroulant
+      dropdown.classList.remove('opened');
+  }
+});
+
+  // Régler la largeur et la hauteur du parent après un délai de 200 millisecondes
   setTimeout(function() {
     parent.style.width = window.getComputedStyle(customSelect, null).getPropertyValue('width');
     parent.style.height = window.getComputedStyle(customSelect, null).getPropertyValue('height');
   }, 200);  
 })
+
+}
+
+export function tagsSearch(tableauValeurs,recipeses) {
+  const results = recipeses.filter(recipe => {
+      // Vérifier si la recette contient tous les éléments du tableau
+      const hasAllValues = tableauValeurs.every(choice => {
+          const hasMatchingIngredient = recipe.ingredients.some(ingredient =>
+              ingredient.ingredient.toLowerCase().includes(choice)
+          );
+
+          const hasMatchingAppliance = recipe.appliance.toLowerCase().includes(choice);
+
+          const hasMatchingUstensil = recipe.ustensils.some(ustensil =>
+              ustensil.toLowerCase().includes(choice)
+          );
+
+          return hasMatchingIngredient || hasMatchingAppliance || hasMatchingUstensil;
+      });
+
+      return hasAllValues;
+  });
+
+ 
+console.log(results);
+  displayTotalRecipes(results)
+clearSelect()
+allIngredients(results)
+allAppareils(results)
+allUstensils(results)
+custumSelect()
+cleanRecipescontenair()
+displayResults(results)
+}
+
+
+
+function updateTags() {
+  if (!tableauValeurs.includes(selectValue)) {
+    tableauValeurs.push(selectValue);
+    console.log(tableauValeurs);}
+}
+
+custumSelect()
+
+
+
 
